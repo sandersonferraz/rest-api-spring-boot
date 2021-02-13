ec57ca3911e3b711e3d54e4a9517d454c1ec69b6
Sanderson Ferraz <sandersonferraz@gmail.com>
2021-02-13 00:02:47 -0300

    First commit

---
+++ src/main/java/br/com/restapispringboot/RestapispringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.restapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapispringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapispringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/restapispringboot/RestapispringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapispringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
