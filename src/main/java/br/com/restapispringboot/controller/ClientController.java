9e0492ba019c7e6ce930a81a653b78f502aa75bf
Sanderson Ferraz <sandersonferraz@gmail.com>
2021-02-13 15:08:00 -0300

    Project finale.

--- src/main/java/br/com/restapispringboot/controller/ClientController.java
+++ src/main/java/br/com/restapispringboot/controller/ClientController.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.restapispringboot.model.Client;
-
 import br.com.restapispringboot.repository.ClientRepository;
 
 @RestController
@@ -22,5 +23,10 @@ public class ClientController {
 	public List<Client> listAll() {
 		return clientRepository.findAll();
 
+	}
+	
+	@PostMapping
+	public Client setClient(@RequestBody Client client) {
+		return clientRepository.save(client);
 	}
 }
